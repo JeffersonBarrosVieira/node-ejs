88370bd81c1d1e369adf2547a2e9225d007ed7d5
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-07-28 20:29:51 -0300

    vercel-node

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.set('view engine', 'ejs') // Definindo qual engine o node vai utilizar (neste caso o Ejs)
 
-app.get('/', (req, res) => {
-    res.render('../views/home')
-})
+// app.get('/', (req, res) => {
+//     res.render('../views/home')
+// })
 
-app.get('/dados', (req, res) => {
+app.get('/', (req, res) => {
     res.json({
         name: 'Jefferson',
         idade: 27
