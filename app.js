39608616b0cfe73c850a1b60d0c7379b59ad04b0
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-07-28 19:55:31 -0300

    ejs-inserido

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs') // Definindo qual engine o node vai utilizar (neste caso o Ejs)
+
+app.get('/', (req, res) => {
+    res.render('../views/home')
+})
+
+app.get('/dados', (req, res) => {
+    res.json({
+        name: 'Jefferson',
+        idade: 27
+    });
+})
+
+app.listen(3000, () => {
+    console.log("Executando na porta 3000!");
+})
