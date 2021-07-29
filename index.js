f933210edd3751dfe884258d03fbc12404938927
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-07-28 23:37:11 -0300

    vercel-ejs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
 app.set('view engine', 'ejs');
 
-app.get('/home', (req, res) => res.render('../views/home'));
+app.get('/home', (req, res) => res.render('./views/home'));
 
 const port = process.env.PORT || 3000;
 
