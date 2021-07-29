3272c722be83b536ea0aaab9db59595526e69783
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-07-28 23:35:34 -0300

    verce-ejs

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get('/about', (req, res) => res.send('About Page Route'));
 
 app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
+app.set('view engine', 'ejs');
+
 app.get('/home', (req, res) => res.render('../views/home'));
 
 const port = process.env.PORT || 3000;
