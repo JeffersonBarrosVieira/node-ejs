89ece7beab1abd00e07ef700cd3a7c6bc29935ef
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-07-28 23:58:48 -0300

    vercel-ejs

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/', (req, res) => res.send('Rota da Página Inicial'));
 
-app.get('/about', (req, res) => res.send('About Page Route'));
+app.get('/about', (req, res) => res.send('Rota da Página Sobre'));
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+app.get('/portfolio', (req, res) => res.send('Rota da Página Portfólio'));
 
-app.get('/teste', (req, res) => res.redirect('home'));
+app.get('/teste', (req,res) => {
+
+    var msg='Funcionou!!';
+
+    ejs.renderFile('views/index.ejs', {msg:msg}, (err, data) => {
+        console.log(err);
+        res.send(data);
+    });
+});
 
 const port = process.env.PORT || 3000;
 
