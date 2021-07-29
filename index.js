91f23cc1ba34f8b4b4a68be997dcfea81a24b94e
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-07-29 09:26:52 -0300

    vercel-ejs

--- index.js
+++ index.js
@@ -4,6 +4,38 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+let ejsOptions = {
+    // delimiter: '?', Adding this to tell you do NOT use this like I've seen in other docs, does not work for Express 4
+    async: true
+};
+
+// The engine is using a callback method for async rendering
+app.engine('ejs', async (path, data, cb) => {
+    try{
+        let html = await ejs.renderFile(path, data, ejsOptions);
+        cb(null, html);
+    }catch (e){
+        cb(e, '');
+    }
+});
+
+const standardResponse = (err, html, res) => {
+    // If error, return 500 page
+    if (err) {
+        console.log(err);
+        // Passing null to the error response to avoid infinite loops XP
+        return res.status(500).render(`login.ejs`, { page : '500', error: err }, (err, html) => standardResponse(null, html, res));
+        // Otherwise return the html
+    } else {
+        return res.status(200).send(html);
+    }
+}
+
+app.get('/login', async (req, res) =>{
+      // layout.ejs is my version of blocking. I pass the page name as an option to render custom pages in the template
+      return await res.render(`login.ejs`, { page : 'login' }, (err, html) => standardResponse(err, html, res));
+})
+
 app.get('/', (req, res) => res.send('Rota da Página Inicial'));
 
 app.get('/about', (req, res) => res.send('Rota da Página Sobre'));
@@ -12,10 +44,12 @@ app.get('/portfolio', (req, res) => res.send('Rota da Página Portfólio'));
 
 app.get('/contact', (req,res) => {
 
-    ejs.renderFile('routes/contact.html', (err, data) => {
-        console.log(err);
-        res.send(data);
-    });
+    // ejs.renderFile('routes/contact.ejs', {msg: "mensagem inserida"}, (err, data) => {
+    //     console.log(err);
+    //     res.send(data);
+    // });
+
+    let html = ejs.renderFile()
 });
 
 const port = process.env.PORT || 5000;
